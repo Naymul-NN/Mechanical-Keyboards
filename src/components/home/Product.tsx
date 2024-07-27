9e6d1f0d91e25414ce00dfdce448fc4e540390e8
Naymul-NN <naymulislam108@gmail.com>
2024-07-27 09:47:58 +0600

    add react rating

--- src/components/home/Product.tsx
+++ src/components/home/Product.tsx
@@ -1,40 +1,53 @@
 import { Link } from "react-router-dom";
 import { useGetProductsQuery } from "../../redux/api/api";
+import Rating from "react-rating";
+import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa";
 
 
 export type producPprops = {
     _id: string;
     image: string;
     title: string;
-    brand:string;
+    brand: string;
     availableQuantity: number;
-    price:number;
+    price: number;
     rating: number
 
-  }
+}
 const Product = () => {
 
-    const {data} = useGetProductsQuery(undefined);
+    const { data } = useGetProductsQuery(undefined);
     console.log(data?.data)
     return (
+        <div>
+
+       
         <div className=" grid grid-cols-3 gap-7 justify-evenly items-center pb-10 ">
-           
-          {data?.data.map((item : producPprops) => (
-                                <div key={item._id} className="card bg-slate-200 shadow-xl">
-                                    <figure><img className="h-[200px] w-[300px] hover:scale-125" src={item.image} alt="Shoes" /></figure>
-                                    <div className="card-body">
-                                        <h2 className="text-black"><strong>Title: </strong>{item.title}</h2>
-                                        <h2 className="text-black"><strong>Brand: </strong>{item.brand}</h2>
-                                        <h2 className="text-black"><strong>Price: </strong>{item.price}$</h2>
-                                        <h2 className="text-black"><strong>InStock: </strong>{item.availableQuantity}</h2>
-                                        <h2 className="text-black"><strong>Rating: </strong>{item.rating}</h2>
-                                        <div className="card-actions ">
-                                        <Link to={`/productDetails/${item._id}`}><button className="btn btn-secondary">see details</button></Link>   
-                                        </div>
-                                    </div>
-                                </div>
-                            ))
-                        }
+
+            {data?.data.map((item: producPprops) => (
+                <div key={item._id} className="card bg-slate-200 shadow-xl">
+                    <figure><img className="h-[200px] w-[300px] hover:scale-125" src={item.image} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="text-black"><strong>Title: </strong>{item.title}</h2>
+                        <h2 className="text-black"><strong>Brand: </strong>{item.brand}</h2>
+                        <h2 className="text-black"><strong>Price: </strong>{item.price}$</h2>
+                        <h2 className="text-black"><strong>InStock: </strong>{item.availableQuantity}</h2>
+                        <h2 className="text-black"><strong>Rating: </strong><Rating
+                            readonly
+                            initialRating={item.rating}
+                            emptySymbol={<FaRegStar className="icon" />}
+                            fullSymbol={<FaStar className="icon text-red-400" />}
+                            placeholderSymbol={<FaStarHalfAlt className="icon" />}
+                        /></h2>
+                        <div className="card-actions ">
+                            <Link to={`/productDetails/${item._id}`}><button className="btn btn-secondary">see details</button></Link>
+                        </div>
+                    </div>
+                </div>
+            ))
+            }
+        </div>
+        <h1 className="text-center pb-5"><button className="btn btn-secondary">View all product</button></h1>
         </div>
     );
 };
--- src/components/home/ProductDetails.tsx
+++ src/components/home/ProductDetails.tsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useGetSingleProductQuery } from "../../redux/api/api";
+import Rating from "react-rating";
+import { FaRegStar, FaStar, FaStarHalfAlt } from "react-icons/fa";
 
 const ProductDetails = () => {
     const { id } = useParams();
@@ -7,15 +9,21 @@ const ProductDetails = () => {
     const { data } = useGetSingleProductQuery(id)
     console.log(data);
     return (
-        <div className="pt-20">
+        <div className="py-24 w-2/3 mx-auto">
             <div className="card bg-slate-200 shadow-xl">
-                <figure><img className="h-[300px] w-[600px] hover:scale-125" src={data?.data?.image} alt="Shoes" /></figure>
-                <div className="card-body">
+                <figure><img className="h-[200px] w-[400px] hover:scale-125" src={data?.data?.image} alt="Shoes" /></figure>
+                <div className="card-body text-center">
                     <h2 className="text-black"><strong>Title: </strong>{data?.data?.title}</h2>
                     <h2 className="text-black"><strong>Brand: </strong>{data?.data?.brand}</h2>
                     <h2 className="text-black"><strong>Price: </strong>{data?.data?.price}$</h2>
                     <h2 className="text-black"><strong>InStock: </strong>{data?.data?.availableQuantity}</h2>
-                    <h2 className="text-black"><strong>Rating: </strong>{data?.data?.rating}</h2>
+                    <h2 className="text-black"><strong>Rating: </strong><Rating
+                        readonly
+                        initialRating={data?.data?.rating}
+                        emptySymbol={<FaRegStar className="icon" />}
+                        fullSymbol={<FaStar className="icon text-red-500" />}
+                        placeholderSymbol={<FaStarHalfAlt className="icon" />}
+                    /></h2>
                 </div>
             </div>
         </div>
